e6c6f7959cf596c26ed9dbbc27f5f38471a06887
renzongyi <renzongyi@xinluomed.com>
2026-09-13 14:56:03 +0800

    Move player debuffs below buff bar customization to ShestakUI_Custom

--- ShestakUI_Custom/Other.lua
+++ ShestakUI_Custom/Other.lua
@@ -110,3 +110,194 @@ else
 	end)
 end
 
+----------------------------------------------------------------------------------------
+-- 4. 玩家 DEBUFF 显示在右上角 BUFF 栏下方并应用 ShestakUI 像素美化
+----------------------------------------------------------------------------------------
+if C.aura and C.aura.player_auras == true then
+	local space = 3
+	local rowbuffs = 16
+
+	-- 1. 屏蔽玩家头像（oUF_Player）上方原有的 Debuffs，防止两处重复显示
+	local function DisablePlayerFrameDebuffs()
+		if _G.oUF_Player and _G.oUF_Player.Debuffs then
+			_G.oUF_Player.Debuffs:Hide()
+			_G.oUF_Player.Debuffs.Show = function() end
+			if _G.oUF_Player.DisableElement then
+				_G.oUF_Player:DisableElement("Debuffs")
+			end
+		end
+	end
+	DisablePlayerFrameDebuffs()
+
+	-- 2. 调整 DEBUFF 锚点（PrivateAnchor）尺寸以匹配 BUFF 栏整行宽度
+	if _G.PrivateAnchor then
+		_G.PrivateAnchor:SetSize((15 * C.aura.player_buff_size) + 42, (C.aura.player_buff_size * 2) + space)
+	end
+
+	-- 3. 动态计算 DEBUFF 锚点位置：未被 /moveui 自定义移动时，自动紧随当前 BUFF 行数下方
+	local function UpdateDebuffsAnchor(numBuffs)
+		if not _G.PrivateAnchor or not _G.BuffsAnchor then return end
+
+		local positionTable = T.CurrentProfile()
+		if positionTable and positionTable["PrivateAnchor"] then return end
+
+		if not numBuffs then
+			numBuffs = BuffFrame and BuffFrame.auraFrames and #BuffFrame.auraFrames or 0
+		end
+		-- BUFF 占 1 行（≤16个）时紧随第 1 行下方；超过 1 行时随第 2 行下移，避免空隙与跳动
+		local rows = numBuffs > rowbuffs and 2 or 1
+		local buffHeight = (rows * C.aura.player_buff_size) + ((rows - 1) * space)
+		local left = T.IsFramePositionedLeft(BuffsAnchor)
+
+		PrivateAnchor:ClearAllPoints()
+		if left then
+			PrivateAnchor:SetPoint("TOPLEFT", BuffsAnchor, "TOPLEFT", 0, -(buffHeight + space + 2))
+		else
+			PrivateAnchor:SetPoint("TOPRIGHT", BuffsAnchor, "TOPRIGHT", 0, -(buffHeight + space + 2))
+		end
+	end
+
+	-- 4. 监听 BUFF 栏更新，动态联动 DEBUFF 垂直位置
+	if BuffFrame and BuffFrame.AuraContainer then
+		hooksecurefunc(BuffFrame.AuraContainer, "UpdateGridLayout", function(_, auras)
+			UpdateDebuffsAnchor(#auras)
+		end)
+	end
+
+	-- 5. 格式化倒计时文本辅助函数
+	local function GetFormattedTime(s)
+		if s >= 86400 then
+			return format("%dd", floor(s / 86400 + 0.5))
+		elseif s >= 3600 then
+			return format("%dh", floor(s / 3600 + 0.5))
+		elseif s >= 60 then
+			return format("%dm", floor(s / 60 + 0.5))
+		end
+		return floor(s + 0.5)
+	end
+
+	local function UpdateDuration(aura, timeLeft)
+		local duration = aura.Duration
+		if duration then
+			if timeLeft and C.aura.show_timer == true then
+				duration:SetVertexColor(1, 1, 1)
+				if canaccessvalue(timeLeft) then
+					duration:SetFormattedText(GetFormattedTime(timeLeft))
+				end
+			else
+				duration:Hide()
+			end
+		end
+	end
+
+	-- 6. 对原生 DebuffFrame 应用 ShestakUI 像素美化、减益类型染色及排版重整
+	if DebuffFrame and DebuffFrame.AuraContainer then
+		hooksecurefunc(DebuffFrame.AuraContainer, "UpdateGridLayout", function(_, auras)
+			UpdateDebuffsAnchor()
+
+			if #auras > 0 then
+				DebuffFrame:Show()
+				DebuffFrame.AuraContainer:Show()
+			end
+
+			local previousBuff, aboveBuff
+			local left = T.IsFramePositionedLeft(BuffsAnchor)
+			for index, aura in ipairs(auras) do
+				aura:SetSize(C.aura.player_buff_size, C.aura.player_buff_size)
+				aura:SetTemplate("Default")
+
+				-- 根据减益类型（魔法、诅咒、中毒、疾病、物理等）为边框着色
+				local debuffBorder = aura.Border or aura.DebuffBorder
+				if debuffBorder then
+					debuffBorder:SetAlpha(0)
+					if not aura.customBorderHook then
+						hooksecurefunc(debuffBorder, "SetVertexColor", function(_, r, g, b)
+							if C.aura.debuff_color_type then
+								aura:SetBackdropBorderColor(r, g, b)
+							else
+								aura:SetBackdropBorderColor(1, 0, 0)
+							end
+						end)
+						aura.customBorderHook = true
+					end
+					if C.aura.debuff_color_type then
+						local r, g, b = debuffBorder:GetVertexColor()
+						if r and g and b then
+							aura:SetBackdropBorderColor(r, g, b)
+						else
+							aura:SetBackdropBorderColor(1, 0, 0)
+						end
+					else
+						aura:SetBackdropBorderColor(1, 0, 0)
+					end
+				else
+					aura:SetBackdropBorderColor(1, 0, 0)
+				end
+
+				aura:ClearAllPoints()
+				if left then
+					if (index > 1) and (mod(index, rowbuffs) == 1) then
+						aura:SetPoint("TOP", aboveBuff, "BOTTOM", 0, -space)
+						aboveBuff = aura
+					elseif index == 1 then
+						aura:SetPoint("TOPLEFT", PrivateAnchor, "TOPLEFT", 0, 0)
+						aboveBuff = aura
+					else
+						aura:SetPoint("LEFT", previousBuff, "RIGHT", space, 0)
+					end
+				else
+					if (index > 1) and (mod(index, rowbuffs) == 1) then
+						aura:SetPoint("TOP", aboveBuff, "BOTTOM", 0, -space)
+						aboveBuff = aura
+					elseif index == 1 then
+						aura:SetPoint("TOPRIGHT", PrivateAnchor, "TOPRIGHT", 0, 0)
+						aboveBuff = aura
+					else
+						aura:SetPoint("RIGHT", previousBuff, "LEFT", -space, 0)
+					end
+				end
+
+				previousBuff = aura
+
+				if aura.Icon then
+					aura.Icon:CropIcon()
+					aura.Icon:SetDrawLayer("BORDER")
+				end
+
+				local duration = aura.Duration
+				if duration then
+					duration:ClearAllPoints()
+					duration:SetPoint("CENTER", 2, 1)
+					duration:SetDrawLayer("ARTWORK")
+					duration:SetFont(C.font.auras_font, C.font.auras_font_size, C.font.auras_font_style)
+					duration:SetShadowOffset(C.font.auras_font_shadow and 1 or 0, C.font.auras_font_shadow and -1 or 0)
+				end
+
+				if not aura.customDurationHook then
+					hooksecurefunc(aura, "UpdateDuration", function(aura, timeLeft)
+						UpdateDuration(aura, timeLeft)
+					end)
+					aura.customDurationHook = true
+				end
+
+				if aura.Count then
+					aura.Count:ClearAllPoints()
+					aura.Count:SetPoint("BOTTOMRIGHT", 2, 0)
+					aura.Count:SetDrawLayer("ARTWORK")
+					aura.Count:SetFont(C.font.auras_font, C.font.auras_font_size, C.font.auras_font_style)
+					aura.Count:SetShadowOffset(C.font.auras_font_shadow and 1 or 0, C.font.auras_font_shadow and -1 or 0)
+				end
+			end
+		end)
+	end
+
+	-- 7. 登录与就绪时完成初始化绑定
+	local initFrame = CreateFrame("Frame")
+	initFrame:RegisterEvent("PLAYER_LOGIN")
+	initFrame:SetScript("OnEvent", function(self)
+		DisablePlayerFrameDebuffs()
+		UpdateDebuffsAnchor()
+		self:UnregisterAllEvents()
+	end)
+end
+
